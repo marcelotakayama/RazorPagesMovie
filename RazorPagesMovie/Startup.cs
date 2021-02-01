1a60e3bd53c53ea0e9851e01b3fa833ba9760ef4
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-01 10:39:15 -0300

    teste

--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -37,7 +37,7 @@ namespace RazorPagesMovie
             options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.a
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
